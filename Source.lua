c6fcc7b6a484174c83e1ed9a75d9b592b9ec40e0
StupidJson <78207462+StupidJson@users.noreply.github.com>
2021-02-02 23:40:30 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,7 +1,12 @@
 local RagdollEngine = 2041312716
+local AlchemyOnline = 5986049891
 
 if game.PlaceId == RagdollEngine then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/StupidJson/RagdollEngine-List/main/source.lua"))()
 else
-  print("No game detect")
+  wait()
+  if game.PlaceId == AlchemyOnline then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/StupidJson/AlchemyOnline-List/main/source.lua"))()
+  else
+    print("No game Detect")
 end
