75c5874b8e61d62e259bf254e690301146da618c
StupidJson <78207462+StupidJson@users.noreply.github.com>
2021-02-02 22:57:56 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,4 +1,7 @@
 local RagdollEngine = 2041312716
 
 if game.PlaceId == RagdollEngine then
-  loadstring()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/StupidJson/RagdollEngine-List/main/source.lua"))()
+else
+  print("No game detect")
+end
