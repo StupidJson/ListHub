cf57f3c35534c46bdfbcf5dcde5fbf180b32b208
StupidJson <78207462+StupidJson@users.noreply.github.com>
2021-02-03 18:37:41 +0800

    source.lua

--- Source.lua
+++ Source.lua
@@ -1,4 +1,4 @@
-local RagdollEngine = 2041312716
+--[[local RagdollEngine = 2041312716
 local AlchemyOnline = 5986049891
 
 if game.PlaceId == RagdollEngine then
@@ -10,4 +10,30 @@ else
   else
     print("No game Detect")
   end
+end]]
+
+GameScripts = {
+  RagdollEngine = {
+		PlaceId = {2041312716},
+		Loadstring = "https://raw.githubusercontent.com/StupidJson/RagdollEngine-List/main/source.lua",
+	},
+  AlchemyOnline = {
+    PlaceId = {5986049891},
+    Loadstring = "https://raw.githubusercontent.com/StupidJson/AlchemyOnline-List/main/source.lua",
+  },
+}
+
+for _, GS in pairs(GameScripts) do
+		if table.find(GS.PlaceId,game.PlaceId) then
+			PromptExecute(GS.Loadstring)
+			break
+		end
+end
+
+function ExploitExecute(ScriptLoadstring)
+  ExecuteScript(ScriptLoadstring)
+end
+
+function ExecuteScript(Code)
+	loadstring(game:HttpGet(Code, true))()
 end
