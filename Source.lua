08f8f0fba617c4299620f97c2f2f237850c5950e
StupidJson <78207462+StupidJson@users.noreply.github.com>
2021-02-03 21:09:17 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,27 +1,27 @@
 GameScripts = {
   RagdollEngine = {
-		PlaceId = {2041312716},
+		PlaceIds = {2041312716},
 		Loadstring = "https://raw.githubusercontent.com/StupidJson/RagdollEngine-List/main/source.lua",
 	},
   AlchemyOnline = {
-		PlaceId = {5986049891},
+		PlaceIds = {5986049891},
 		Loadstring = "https://raw.githubusercontent.com/StupidJson/AlchemyOnline-List/main/source.lua",
 	},
 }
 
-for _, GS in pairs(GameScripts) do
-		if table.find(GS.PlaceId,game.PlaceId) then
-			ExploitExecute(GS.Loadstring)
-			break
-		end
+function ExecuteScript(Code)
+	loadstring(game:HttpGet(Code))()
+end
+
+function ExploitExecute(ScriptLoadstring)
+    ExecuteScript(ScriptLoadstring)
 end
 
 wait(0.4)
 
-function ExploitExecute(ScriptLoadstring)
-  ExecuteScript(ScriptLoadstring)
-end
-
-function ExecuteScript(Code)
-	loadstring(game:HttpGet(Code, true))()
+for _, Test in pairs(GameScripts) do
+		if table.find(Test.PlaceIds, game.PlaceId) then
+			ExploitExecute(Test.Loadstring)
+			break
+		end
 end
